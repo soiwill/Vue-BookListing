83331971ed470191ff78ecfd3ff8cd7912af5c37
Sol Lee <chenlee@microsoft.com>
2019-03-02 01:16:33 +0800

    module 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,15 @@
 <template>
 <div>
     <h1>{{title}}</h1>
+    <ul>
+        <li>Book 1</li>
+        <li>Book 2</li>
+        <li>Book 3</li>
+    </ul>
+    <ul>
+        <li v-for="book in books">{{book.title}}... Content:{{book.author}}</li>
+    </ul>
+    
 </div>
 
 </template>
@@ -10,7 +19,12 @@ export default {
     name: "BookList",
     data(){
         return {
-            title: "All Books"
+            title: "All Books",
+            books: [
+                {title:"Sol", author:"Sol Author"},
+                {title:"Yvonne", author:"Yvonne Author"},
+                {title:"Daughter", author:"Daughter"}
+            ]
         }
     }
 }
@@ -19,5 +33,13 @@ export default {
 <style>
 h1, h2 {
     font-weight: normal
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: block;
+    padding: 0 10px;
 }
 </style>
